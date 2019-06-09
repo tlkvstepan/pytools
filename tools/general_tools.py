42236f2457e8aa04e859a1cbdce9128d39d69d08
tulyakov <tulyakov.stepan@gmail.com>
2019-06-09 14:48:50 +0200

    Added "dilate" function to "general_tools".

--- tools/general_tools.py
+++ tools/general_tools.py
@@ -4,7 +4,27 @@
 # See the LICENSE.TXT file for more details.
 from collections import defaultdict
 
+from scipy.ndimage import morphology
+import numpy as np
 import torch as th
+
+
+def dilate(image, window_size):
+    """Returns dilated image.
+
+    Args:
+        image: is a boolen tensor of size (height x width) image
+               that will be dilated.
+        window_size: size of the window that will be used for the
+                     dilation.
+    """
+    dilation_window = np.ones((window_size, window_size), dtype=bool)
+    dilated_image = th.from_numpy(
+        morphology.binary_dilation(image.cpu().numpy(),
+                                   dilation_window).astype(np.uint8))
+    if image.is_cuda:
+        return dilated_image.cuda()
+    return dilated_image
 
 
 def create_meshgrid(width, height, is_cuda):
